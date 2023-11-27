40a1db0b4234fc1f52a9d63ba304d2a19a0c4fe3
Artur Wiśniewski <artixbtw@gmail.com>
2023-11-27 11:19:20 +0100

    create json data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,15 @@
+import styled from "styled-components";
+import { DataTable } from "../components/DataTable";
+
+const Container = styled.div`
+  margin-left: 200px;
+`;
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <DataTable />
+    </Container>
+  );
   1;
 };
